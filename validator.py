562e4e949378541319976e639d21c6d98c583a93
Gerifield <gergo254@gmail.com>
2012-01-21 00:34:24 +0100

    Argument feldolgozas tervezet #1

---
+++ validator.py
@@ -0,0 +1,9 @@
+import argparse, sys
+
+def main():
+  parser = argparse.ArgumentParser(description='Sitevalidator alkalmazas, weboldalak teljes validalasahoz.')
+  #parser.print_help()
+
+
+if __name__ == "__main__":
+  sys.exit(main())
