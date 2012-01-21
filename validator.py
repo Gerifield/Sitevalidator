90db4680cc1244faaa38a44bfe370117717803f2
Gerifield <gergo254@gmail.com>
2012-01-21 01:57:48 +0100

    suds megsem fog kelleni, tul sok

--- validator.py
+++ validator.py
@@ -2,7 +2,7 @@ import argparse, sys
 
 def main():
   parser = argparse.ArgumentParser(description='Sitevalidator alkalmazas, weboldalak teljes validalasahoz.')
-  #parser.print_help()
+  parser.print_help()
 
 
 if __name__ == "__main__":
