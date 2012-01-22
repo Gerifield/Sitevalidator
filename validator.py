23d534394c8c7b70c9279d8749e6dffd0391c919
Gerifield <gergo254@gmail.com>
2012-01-22 01:22:24 +0100

    Teszteles #1

--- validator.py
+++ validator.py
@@ -1,8 +1,15 @@
-import argparse, sys
+import argparse, sys, httplib
+
 
 def main():
   parser = argparse.ArgumentParser(description='Sitevalidator alkalmazas, weboldalak teljes validalasahoz.')
   parser.print_help()
+  churl = "http://people.inf.elte.hu/vzoli"
+  conn = httplib.HTTPConnection("validator.w3.org")
+  conn.request("GET", "/check?uri="+churl+"&output=soap12")
+  resp = conn.getresponse()
+
+  print resp.getheaders()
 
 
 if __name__ == "__main__":
