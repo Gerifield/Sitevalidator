c1b76d30e0b1ff14d2fca0642f80b63ebc19d39a
Gerifield <gergo254@gmail.com>
2012-01-27 19:58:32 +0100

    beautiful soup teszt, W3C-t feldolgoz mar

--- validator.py
+++ validator.py
@@ -12,7 +12,19 @@ def main():
   headers = r.info() # -> headers['X-W3C-Validator-*']
   data = r.read()
   
-
+  soup = BeautifulSoup(data, convertEntities=BeautifulSoup.HTML_ENTITIES)
+  errors = soup.findAll('m:error')
+  for error in errors:
+    print 'Sor: ', error('m:line')[0].string
+    print 'Oszlop: ', error('m:col')[0].string
+    print 'Hiba: ', error('m:message')[0].string
+    print "------------------------------"
+  
+  print 'DOCTYPE: ', soup.first('m:doctype').string #doctype
+  print 'CHARSET: ', soup.first('m:charset').string #encoding
+  print 'VALID: ',soup.first('m:validity').string #validity
+  print 'ERRORS: ', soup.first('m:errorcount').string
+  print 'WARNINGS: ', soup.first('m:warningcount').string
 
 if __name__ == "__main__":
   sys.exit(main())
