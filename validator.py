1e85d55092a935871e9ad9fd6b8ad7efe74017bc
Gerifield <gergo254@gmail.com>
2012-01-27 20:46:41 +0100

    validator atirva az up apihoz

--- validator.py
+++ validator.py
@@ -1,5 +1,7 @@
-import argparse, sys, urllib2
-from BeautifulSoup import BeautifulSoup
+import argparse
+import sys
+import W3cSoapApi
+
 
 
 def main():
@@ -7,24 +9,33 @@ def main():
   parser.print_help()
   
   churl = "http://people.inf.elte.hu/vzoli" #web URL
-  req = urllib2.Request("http://validator.w3.org/check?uri="+churl+"&output=soap12") #validation...
-  r = urllib2.urlopen(req)
-  headers = r.info() # -> headers['X-W3C-Validator-*']
-  data = r.read()
+  #req = urllib2.Request("http://validator.w3.org/check?uri="+churl+"&output=soap12") #validation...
+  #r = urllib2.urlopen(req)
+  #headers = r.info() # -> headers['X-W3C-Validator-*']
+  #data = r.read()
   
-  soup = BeautifulSoup(data, convertEntities=BeautifulSoup.HTML_ENTITIES)
-  errors = soup.findAll('m:error')
-  for error in errors:
-    print 'Sor: ', error('m:line')[0].string
-    print 'Oszlop: ', error('m:col')[0].string
-    print 'Hiba: ', error('m:message')[0].string
-    print "------------------------------"
+  #soup = BeautifulSoup(data, convertEntities=BeautifulSoup.HTML_ENTITIES)
+  #errors = soup.findAll('m:error')
+  #for error in errors:
+  #  print 'Sor: ', error('m:line')[0].string
+  #  print 'Oszlop: ', error('m:col')[0].string
+  #  print 'Hiba: ', error('m:message')[0].string
+  #  print "------------------------------"
   
-  print 'DOCTYPE: ', soup.first('m:doctype').string #doctype
-  print 'CHARSET: ', soup.first('m:charset').string #encoding
-  print 'VALID: ',soup.first('m:validity').string #validity
-  print 'ERRORS: ', soup.first('m:errorcount').string
-  print 'WARNINGS: ', soup.first('m:warningcount').string
+  #print 'DOCTYPE: ', soup.first('m:doctype').string #doctype
+  #print 'CHARSET: ', soup.first('m:charset').string #encoding
+  #print 'VALID: ',soup.first('m:validity').string #validity
+  #print 'ERRORS: ', soup.first('m:errorcount').string
+  #print 'WARNINGS: ', soup.first('m:warningcount').string
+  validator = W3cSoapApi.W3cSoapApi(churl)
+  validator.parse()
+  print validator.getHeaders()
+  print "------------------"
+  print "Doctype: ", validator.getDoctype()
+  print "Errors: ", validator.getErrorNum()
+  print "Warnings: ", validator.getWarningNum()
+  print "Valid? ", validator.isValid()
+  
 
 if __name__ == "__main__":
   sys.exit(main())
