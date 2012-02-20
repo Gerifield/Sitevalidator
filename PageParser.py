8bde547bba40580749219805d4a40338341be386
Gerifield <gergo254@gmail.com>
2012-02-21 00:15:39 +0100

    uj fuggvenyek es finomitas a szerkezeten, keszurl-ek tarolasa

--- PageParser.py
+++ PageParser.py
@@ -1,5 +1,40 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
+from urlparse import urlparse
 
 class PageParser:
+  urllist = []
+  finurl = []
+  baseurl = ""
   
+  def __init__(self, url):
+    self.addUlr(url)
+    url = urlparse(url)
+    self.baseurl = url.netloc #kiszedjuk az url cimet
+  
+  def addUlr(self, url):
+    if self.finurl.count(url) == 0: #ha eddig nem dolgoztuk mar fel
+      self.urllist.append(url)
+
+  def isUrl(self):
+    return len(self.urllist) > 0
+  def nextUrl(self):
+    ret = self.urllist.pop()
+    self.finurl.append(ret) #hozzaadjuk a feldolgozotthoz
+    return ret
+  def isLocal(self, url):
+    url = urlparse(url)
+    if self.baseurl == url.netloc:
+      return True
+    else:
+      return False
+  def parsePage(self):
+    if self.isUrl():
+      u = self.nextUrl()
+      html = urllib2.urlopen(u).read()
+      soup = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES)
+      
+      print soup.findAll('a')
+      
+    else:
+      return False
