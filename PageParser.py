6e7947d042147e4083a48ca187b19ec5d3790f2b
Gerifield <gergo254@gmail.com>
2012-02-21 01:09:10 +0100

    URL feldolgozas megvan, vegzodesek figyelese hozzaadva

--- PageParser.py
+++ PageParser.py
@@ -7,29 +7,41 @@ class PageParser:
   urllist = []
   finurl = []
   baseurl = ""
+  allowedEnds = ["/", ".php", ".htm", ".html", ".asp"]
+  
   
   def __init__(self, url):
-    self.addUlr(url)
+    self.addUrl(url)
     url = urlparse(url)
     self.baseurl = url.scheme + "://" + url.netloc #kiszedjuk az url cimet
     #print self.baseurl
   
-  def addUlr(self, url):
+  def addUrl(self, url):
     if self.finurl.count(url) == 0: #ha eddig nem dolgoztuk mar fel
       self.urllist.append(url)
 
   def hasUrl(self):
     return len(self.urllist) > 0
+    
   def nextUrl(self):
     ret = self.urllist.pop()
     self.finurl.append(ret) #hozzaadjuk a feldolgozotthoz
     return ret
+    
   def isLocal(self, url):
     url = urlparse(url)
     if self.baseurl == url.scheme + "://" + url.netloc:
       return True
     else:
       return False
+
+  def checkEnding(self, url):
+    for end in self.allowedEnds:
+      if url.endswith(end):
+        #print url
+        return True
+    return False
+
   def parsePage(self): # minden aloldalon vegigmegy
     while self.hasUrl():
       u = self.nextUrl()
@@ -38,7 +50,9 @@ class PageParser:
       
       tags = soup.findAll('a', attrs={'href': re.compile("^"+self.baseurl)}) #csak a lokalis url-eket szedjuk ki
       for tag in tags:
-        print tag['href'] #URL feldolgozas
-      
+        #print tag['href'] #URL feldolgozas
+        if self.checkEnding(tag['href']):
+          print "Good: "+tag['href']
+          self.addUrl(tag['href'])
     else:
       return True
