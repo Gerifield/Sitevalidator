ca100282f83c206d70c0751c7c3c4c6019365c92
Gerifield <gergo254@gmail.com>
2012-04-03 18:45:37 +0200

    Loginra atiranyitas hozzaadva!

--- frontend/application/controllers/main.php
+++ frontend/application/controllers/main.php
@@ -2,28 +2,31 @@
 
 class Main extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+
 	public function index()
 	{
-		$this->load->view('header');
-    $this->load->view('menu');
-    $this->load->view('footer');
+    if(!$this->session->userdata('logged_in')){
+    
+      $this->load->view('header');
+      $this->load->view('menu');
+      $this->load->view('footer');
+    }else{
+      redirect("main/login");
+    }
 	}
+  
+  public function login(){
+		$this->load->view('header');
+    $this->load->view('content_login');
+    $this->load->view('footer');
+  }
+  
+    public function logout(){
+        $this->session->set_userdata("logged_in", false);
+        $this->session->sess_destroy();
+        redirect("main/index");
+    }
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file main.php */
+/* Location: ./application/controllers/main.php */
