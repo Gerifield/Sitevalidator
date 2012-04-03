61415b7c42ce9de163b2c5c3ee693260e1a623cb
Gerifield <gergo254@gmail.com>
2012-04-03 19:58:47 +0200

    Login ellenorzes hozzaadva.

--- frontend/application/models/dbmodel.php
+++ frontend/application/models/dbmodel.php
@@ -11,7 +11,14 @@ class Dbmodel extends CI_Model {
     }
     
     function checkLogin($user, $pass){
-      
+      $this->db->where("user", $user);
+      $this->db->where("pass", $pass);
+      $q = $this->db->get("users");
+      if($q->num_rows() == 1){
+        return true;
+      }else{
+        return false;
+      }
     }
 
 }
