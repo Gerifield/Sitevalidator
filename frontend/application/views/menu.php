55c8966d6d3ee4150a4a4c20b2fc0f53c4098b22
Gerifield <gergo254@gmail.com>
2012-04-03 20:18:40 +0200

    Eger ravitel kezelese.

--- frontend/application/views/menu.php
+++ frontend/application/views/menu.php
@@ -1,8 +1,6 @@
-           <div class="menu">
-                <a class="menu_button_sel" href="index.html">Főoldal</a>
-                <a class="menu_button" href="project.html">Projektek</a>
-                <a class="menu_button" href="rolam.html">Rólam</a>
-                <a class="menu_button" href="kapcsolat.html">Kapcsolat</a>
-                <a class="menu_button" href="http://blog.gerifield.hu">Microblog</a>
-            </div>
-            <div class="content">
+<div class="menu">
+    <a class="menu_button" href="<?php echo site_url("main/index"); ?>">Főoldal</a>
+    <a class="menu_button" href="<?php echo site_url("main/profile"); ?>">Profil</a>
+    <a class="menu_button" href="<?php echo site_url("main/logout"); ?>">Kilépés</a>
+</div>
+<div class="content">
