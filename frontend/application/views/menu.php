b46d093064463af2ce39533ea1f9479d1a2e5d34
Gerifield <gergo254@gmail.com>
2012-04-03 18:04:38 +0200

    Alap view fileok letrehozasa. (Oldal sablon)

---
+++ frontend/application/views/menu.php
@@ -0,0 +1,8 @@
+           <div class="menu">
+                <a class="menu_button_sel" href="index.html">Főoldal</a>
+                <a class="menu_button" href="project.html">Projektek</a>
+                <a class="menu_button" href="rolam.html">Rólam</a>
+                <a class="menu_button" href="kapcsolat.html">Kapcsolat</a>
+                <a class="menu_button" href="http://blog.gerifield.hu">Microblog</a>
+            </div>
+            <div class="content">
