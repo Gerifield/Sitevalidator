69b2b14056f528feb13559d18839fc928a0c54fa
Gerifield <gergo254@gmail.com>
2012-04-04 17:58:12 +0200

    Adatok kiirasa.

--- frontend/application/views/content_profile.php
+++ frontend/application/views/content_profile.php
@@ -5,7 +5,8 @@
       ?>
       <form method="post" action="">
       <table style="margin-left: auto; margin-right: auto;">
-      <tr><td>E-mail:</td><td><input type="text" name="email" value="<?php if($email){echo $email; } ?>" /></td></tr>
+      <tr><td>Felhasználó név:</td><td><?php echo $this->session->userdata('user'); ?></td></tr>
+      <tr><td>E-mail:</td><td><input type="text" name="email" value="<?php echo $email; ?>" /></td></tr>
       <tr><td>Jelszó*:</td><td><input type="password" name="pass1" /></td></tr>
       <tr><td>Jelszó újra*:</td><td><input type="password" name="pass2" /></td></tr>
       <tr><td></td><td><input type="submit" value="Módosít" /></td></tr>
