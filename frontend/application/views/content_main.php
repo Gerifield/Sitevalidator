5ba40713da46a897858aadbd863ea8f906200430
Gerifield <gergo254@gmail.com>
2012-04-08 20:22:32 +0200

    Tablazat elkeszitese.

--- frontend/application/views/content_main.php
+++ frontend/application/views/content_main.php
@@ -3,15 +3,37 @@
 </form>
 <br />
 <table class="proc_table">
-<tr><th>ID</th><th>URL</th><th>Futtatás</th><th>Állapot</th></tr>
+<tr>
+<th>ID</th>
+<th>URL</th>
+<th>Futtatás</th>
+<th>Állapot</th>
+<th>HTML Validitás</th>
+<th>HTML Doctype</th>
+<th>HTML Hiba</th>
+<th>HTML Figyelmeztetés</th>
+<th>CSS Validitás</th>
+<th>CSS Doctype</th>
+<th>CSS Hiba</th>
+<th>CSS Figyelmeztetés</th>
+</tr>
 <?php
   foreach($datalist as $row):
 ?>
 <tr class="clickable">
 <td><?php echo $row['id']; ?></td>
 <td><?php echo $row['url']; ?></td>
-<td class="runtime"><?php echo $row['runtime']; ?></td>
-<td><?php echo $row['state']; ?></td>
+<td><?php echo $row['runtime']; ?></td>
+<td><?php if($row['state'] == 0){echo "Várakozik"; }elseif($row['state'] == 1){ echo "Futtatás..."; }elseif($row['state'] == 2){ echo "Kész"; } ?></td>
+<td><?php if($row["htmlvalidity"]){ echo "Invalid"; }else{ echo "Valid"; }; ?></td>
+<td><?php if(empty($row["htmldoctype"])){ echo "Ismeretlen"; }else{ echo $row["htmldoctype"]; }; ?></td>
+<td><?php echo $row["htmlerrornum"]; ?></td>
+<td><?php echo $row["htmlwarningnum"]; ?></td>
+<td><?php if($row["cssvalidity"]){ echo "Invalid"; }else{ echo "Valid"; }; ?></td>
+<td><?php if(empty($row["cssdoctype"])){ echo "Ismeretlen"; }else{ echo $row["htmldoctype"]; }; ?></td>
+<td><?php echo $row["csserrornum"]; ?></td>
+<td><?php echo $row["csswarningnum"]; ?></td>
+<td><a href="#">Töröl</a> <a href="#">Módosít</a></td>
 </tr>
 <?php
   endforeach;
