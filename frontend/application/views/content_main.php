fcad5c0bf90c0128087a92df4ecbc59d7f5a3437
unknown <Gerifield@Gerifield-PC.(none)>
2012-04-12 17:18:38 +0200

    View a folyamat adat bevitelhez.

--- frontend/application/views/content_main.php
+++ frontend/application/views/content_main.php
@@ -1,6 +1,11 @@
+<?php
+if(isset($errormsg)){
+	echo '<div class="errormsg">'.$errormsg.'</div>';
+}
+?>
 <form method="post" action="">
-<p>URL*: <input type="text" name="inurl" size="70" /> Időpont: <input type="text" name="runtime" /><input type="submit" value="Hozzáad" />
-<br /></p><div style="font-size:85%">*:Teljes url cím, http-vel és .html, .html, .php vagy .asp végződéssel!</div>
+<p>URL*: <input type="text" name="inurl" value="<?php echo $inurl; ?>" size="70" /> Időpont: <input type="text" name="runtime" value="<?php echo $runtime; ?>" /><input type="submit" value="Hozzáad" />
+<br /></p><div style="font-size:85%">*:Teljes url cím, http-vel és .htm, .html, .php vagy .asp végződéssel!</div>
 </form>
 <br />
 <table class="proc_table">
@@ -18,7 +23,7 @@
 <tr class="clickable">
 <td><?php echo $row['id']; ?></td>
 <td><?php echo $row['url']; ?></td>
-<td><?php echo $row['runtime']; ?></td>
+<td><?php echo date("Y-m-d H:i", $row['runtime']); ?></td>
 <td><?php if($row['state'] == 0){echo "Várakozik"; }elseif($row['state'] == 1){ echo "Futtatás..."; }elseif($row['state'] == 2){ echo "Kész"; } ?></td>
 <td><?php if($row["htmlvalidity"] == 0){ echo "Invalid"; }else{ echo "Valid"; }; ?></td>
 <td><?php if($row["cssvalidity"] == 0){ echo "Invalid"; }else{ echo "Valid"; }; ?></td>
--- frontend/application/views/content_procdetails.php
+++ frontend/application/views/content_procdetails.php
@@ -2,11 +2,18 @@
 if(!$datalist):
   echo "Ismeretlen ID vagy tiltott hozzáférés.";
 else:
+
+  if(isset($errormsg)){
+    echo '<div class="errormsg">'.$errormsg.'</div>';
+  }
 ?>
+<form method="post" action="">
 <table class="proc_table" style="width: 100%">
 <tr><td>ID:</td><td><?php echo $datalist["id"]; ?></td></tr>
-<tr><td>URL:</td><td><?php echo $datalist["url"]; ?></td></tr>
-<tr><td>Futtatás ideje:</td><td><?php echo $datalist["runtime"]; ?></td></tr>
+
+<tr><td>URL:</td><td><input type="text" name="inurl" value="<?php echo $datalist["url"]; ?>" /></td></tr>
+<tr><td>Futtatás ideje:</td><td><input type="text" name="runtime" value="<?php echo date("Y-m-d H:i", $datalist["runtime"]); ?>" /> <input type="submit" value="Módosítás" /></td></tr>
+
 <tr><td>Állapot:</td><td><?php if($datalist['state'] == 0){echo "Várakozik"; }elseif($datalist['state'] == 1){ echo "Futtatás..."; }elseif($datalist['state'] == 2){ echo "Kész"; } ?></td></tr>
 
 <tr><td>HTML Validitás:</td><td><?php if($datalist["htmlvalidity"] == 0){ echo "Invalid"; }else{ echo "Valid"; }; ?></td></tr>
@@ -19,6 +26,7 @@ else:
 <tr><td>CSS hibák:</td><td><?php echo $datalist["csserrornum"]; ?></td></tr>
 <tr><td>CSS figyelmeztetések:</td><td><?php echo $datalist["csswarningnum"]; ?></td></tr>
 </table>
+</form>
 <?php
 endif;
 ?>
