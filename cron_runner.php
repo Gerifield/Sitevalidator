e1e44670dda0ced6cdaae4dc5d722ebd3468afcb
Gerifield <gergo254@gmail.com>
2012-04-16 21:04:52 +0200

    SQL keres megirasa.

--- cron_runner.php
+++ cron_runner.php
@@ -4,5 +4,18 @@ include('frontend/application/config/database.php'); //nem szép, de hatékonyabb!
 
 $mysqli = new mysqli($db['default']['hostname'], $db['default']['username'], $db['default']['password'], $db['default']['database']);
 
+$time = time() + (5*60); //mennyi ideig nezzen elore a meres (masodpercben)
+//5*60 -> 5 perc
+
+if($res = $mysqli->query("SELECT * FROM processes WHERE state = 0 AND runtime < ".$time)){
+//minden kérésre ami várakozik és az aktuális idõ +5 percen belül futtatandó
+  
+  while($row = $res->fetch_array()){
+    echo $row['id']."<br />";
+  }
+}else{
+  echo $mysqli->error; //debughoz
+}
+
 $mysqli->close();
 ?>
