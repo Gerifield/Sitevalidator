e2d5e4ff24aed8d7ec0e79a8e63c31e8780110e0
Gerifield <gergo254@gmail.com>
2012-04-16 19:26:09 +0200

    Regularis kifejezes a vegzodes ellenorzesre.

--- frontend/application/controllers/main.php
+++ frontend/application/controllers/main.php
@@ -15,11 +15,9 @@ class Main extends CI_Controller {
         if($stamp && $stamp != -1){ //regen -1 volt 5.1.0 elott
         //print "OK";
         //print $stamp." -> ".date("Y-m-d H:i", $stamp);
-        $vegzodes3 = array("htm", "php", "asp");
-        $vegzodes4 = array("html");
         
-        if(preg_match("^http", $inurl)){
-          if(in_array(substr($inurl, -3), $vegzodes3) or in_array(substr($inurl, -4), $vegzodes4)){
+        if(preg_match("/^http/", $inurl)){
+          if(preg_match("/(htm|html|php|asp)$/", $inurl)){
             
             $data["inurl"] = "";
             $data["runtime"] = "";
