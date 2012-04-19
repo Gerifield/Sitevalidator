4e5889bff930ecbf8880112eddf6e4bde6ad024a
Gerifield <gergo254@gmail.com>
2012-04-19 21:31:19 +0200

    Validsag javitasa a reszletezo oldalon.

--- frontend/application/views/content_procdetails.php
+++ frontend/application/views/content_procdetails.php
@@ -11,10 +11,10 @@ else:
   }
 ?>
 <form method="post" action="">
-<input type="text" name="inurl" value="<?php echo $datalist["url"]; ?>" size="70" />
+<div><input type="text" name="inurl" value="<?php echo $datalist["url"]; ?>" size="70" />
 <input type="text" name="runtime" value="<?php echo date("Y-m-d H:i", $datalist["runtime"]); ?>" />
 <input type="hidden" name="sendform" value="true" />
-<input type="submit" value="Módosítás" />
+<input type="submit" value="Módosítás" /></div>
 </form>
 <?php
 
