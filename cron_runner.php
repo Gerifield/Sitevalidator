0cdf6f624de52e2840241c2952e37ba82a2de333
Gerifield <gergo254@gmail.com>
2012-04-20 17:35:46 +0200

    Linuxon tesztelt es mukodo futtato php.

--- cron_runner.php
+++ cron_runner.php
@@ -1,8 +1,12 @@
 <?php
 define('BASEPATH', ''); //Kell, hogy hozzáférhessen a database.php-hez
+//include('application/config/database.php'); //nem szép, de hatékonyabb!
 include('frontend/application/config/database.php'); //nem szép, de hatékonyabb!
 
+//$callback_url = "http://127.0.0.1/index.php/bg/index";
 $callback_url = "http://127.0.0.1/SZAKDOLI/frontend/index.php/bg/index";
+//$file_path = "/home/gerifield/Sitevalidator/validator.py";
+$file_path = "validator.py";
 
 $mysqli = new mysqli($db['default']['hostname'], $db['default']['username'], $db['default']['password'], $db['default']['database']);
 
@@ -13,12 +17,15 @@ if($res = $mysqli->query("SELECT * FROM processes WHERE state = 0 AND runtime < 
 //minden kérésre ami várakozik és az aktuális idõ +5 percen belül futtatandó
   
   while($row = $res->fetch_array()){
-    echo $row['id']."<br />";
+//    echo $row['id']."<br />";
     $mysqli->query("UPDATE processes SET state=1 WHERE id = ".$row['id']);
     
     //TODO: futtatás megoldani
     exec('C:\\Python27\\python.exe validator.py  --callback '.$callback_url.'/'.$row['token'].' '.$row['url'].' > dump.txt &');
-    
+   //passthru('/usr/bin/python '.$file_path.' --format silent --callback '.$callback_url.'/'.$row['token'].' '.$row['url'].' &');
+   //echo shell_exec('/usr/bin/python '.$file_path.' --format silent --callback '.$callback_url.'/'.$row['token'].' '.$row['url'].' > /dev/null & echo "'.$row['id'].' started"');
+  //echo $file_path. " -> ".$callback_url;
+//  echo '/usr/bin/python '.$file_path.' --format silent --callback '.$callback_url.'/'.$row['token'].' '.$row['url'].' ';  
   }
 }else{
   echo $mysqli->error; //debughoz
