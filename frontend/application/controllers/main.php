9a36489ecac65dc41128a22d5fd7c9dcf051bd2d
Gerifield <gergo254@gmail.com>
2012-05-02 18:35:30 +0200

    Hivatkozas a valtozora javitva.

--- frontend/application/controllers/main.php
+++ frontend/application/controllers/main.php
@@ -20,7 +20,7 @@ class Main extends CI_Controller {
           //print "OK";
           //print $stamp." -> ".date("Y-m-d H:i", $stamp);
             if(preg_match("/^http/", $inurl)){
-              if(preg_match($ext_regex, $inurl)){
+              if(preg_match($this->ext_regex, $inurl)){
                 
                 if(!is_numeric($data["repeat"])){ //ha hamis vagy nem szám, akkor nullázzuk
                   $data["repeat"] = 0;
@@ -214,7 +214,7 @@ class Main extends CI_Controller {
             //print $stamp." -> ".date("Y-m-d H:i", $stamp);
             
             if(preg_match("/^http/", $inurl)){
-              if(preg_match($ext_regex, $inurl)){
+              if(preg_match($this->ext_regex, $inurl)){
 
                 if(!is_numeric($data["repeat"])){ //ha hamis vagy nem szám, akkor nullázzuk
                   $data["repeat"] = 0;
